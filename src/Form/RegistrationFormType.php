0c4dc935800617d1d85fb8218c0960e23dbf5901
etupdt <denis.tavernier@gmail.com>
2025-10-05 11:47:03 +0200

    Feat: add notnull fields to form login

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,6 +18,10 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
+            ->add('pseudo')
+            ->add('nom')
+            ->add('prenom')
+            ->add('photo')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
