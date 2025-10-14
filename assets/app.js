d0277ca9b68e073dd25d6f4ff2287785fecbb5df
etupdt <denis.tavernier@gmail.com>
2025-10-14 08:53:16 +0200

    Config: install webpack

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,31 @@
-import './bootstrap.js';
 /*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+* Welcome to your app's main JavaScript file!
+*
+* This file will be included onto the page via the importmap() Twig function,
+* which should already be in your base.html.twig.
+*/
 import './styles/app.scss';
+// import './bootstrap.js';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// import 'bootstrap';
+// import bsCustomFileInput from 'bs-custom-file-input';
+
+// bsCustomFileInput.init();
+
+
+// console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// const $ = require('jquery');
+// require('bootstrap');
+
+// $(document).ready(function() {
+//     $('[data-toggle="popover"]').popover();
+// });
+
+document.addEventListener('turbo:load', function (e) {
+    // this enables bootstrap tooltips globally
+    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    });
+});
