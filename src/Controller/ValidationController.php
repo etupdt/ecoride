0a8d4dd3b1338c91ba562f5f6eb890e618da7d2c
etupdt <denis.tavernier@gmail.com>
2025-11-16 16:06:47 +0100

    Feat: add avis management

---
+++ src/Controller/ValidationController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\CovoiturageRepository;
+use App\Repository\ParticipationRepository;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use App\Document\Avis;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\ValidationFormType;
+
+final class ValidationController extends AbstractController
+{
+    #[Route('/validation/{id}', name: 'app_valider_covoiturage')]
+    public function index(
+        int $id, 
+        Request $request, 
+        EntityManagerInterface $entityManager,
+        CovoiturageRepository $covoiturageRepository, 
+        ParticipationRepository $participationRepository, 
+        DocumentManager $documentManager,
+    ): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        
+        $covoiturage = $covoiturageRepository->find($id);
+        
+        $participation = $participationRepository->findOneBy([
+            'passager' => $user,
+            'covoiturage' => $covoiturage
+        ]);
+
+        $form = $this->createForm(ValidationFormType::class);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $participation->setStatut($form->get('validation')->getData());
+            $participation->setCommentaire($form->get('commentaire')->getData());
+
+            $chauffeur = $covoiturage->getVoiture()->getChauffeur();
+
+            $avis = new Avis();
+            $avis->setChauffeur($chauffeur->getId());
+            $avis->setAvis($form->get('avis')->getData());
+            $avis->setModere(false);
+            $avis->setValide(false);
+            $avis->setNote($form->get('note')->getData());
+
+            $documentManager->persist($avis);   
+            $documentManager->flush();
+
+            $note = $documentManager->getRepository(Avis::class)->findNoteByChauffeur($chauffeur);
+            $chauffeur->setNote($note[0]['note']);
+
+            $entityManager->persist($chauffeur);
+            $entityManager->persist($participation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_participations');
+
+        }
+
+        return $this->render('validation/index.html.twig', [
+            'controller_name' => 'ValidationController',
+            'validationForm' => $form,
+            'user' => $user,
+        ]);
+
+    }
+    
+    #[Route('/avis/d/{id}', name: 'app_invalid_avis')]
+    public function delete(string $id, DocumentManager $documentManager): Response
+    {
+
+        $avis = $documentManager->getRepository(Avis::class)->find($id);
+        
+        $avis->setModere(true);
+        $avis->setValide(false);
+
+        $documentManager->persist($avis);
+        $documentManager->flush();
+
+        return $this->redirectToRoute('app_avis');
+
+    }
+
+    #[Route('/avis', name: 'app_avis', methods: ['GET'])]
+    public function avis(DocumentManager $documentManager): Response
+    {
+
+        $avis = $documentManager->getRepository(Avis::class)->findBy([
+            'modere' => false
+        ]);
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return $this->render('avis/list.html.twig', [
+            'controller_name' => 'ValidationController',
+            'user' => $user,
+            'avis' => $avis
+        ]);
+
+    }
+
+    #[Route('/contentieux', name: 'app_contentieux', methods: ['GET'])]
+    public function contentieux(
+        ParticipationRepository $participationRepository, 
+    ): Response
+    {
+
+        $kos = $participationRepository->findBy([
+            'statut' => 'ko'
+        ]);
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return $this->render('contentieux/list.html.twig', [
+            'controller_name' => 'ValidationController',
+            'user' => $user,
+            'participations' => $kos
+        ]);
+
+    }
+
+    #[Route('/contentieux/u/{id}', name: 'app_regle_contentieux')]
+    public function regle(
+        int $id, 
+        ParticipationRepository $participationRepository,
+        EntityManagerInterface $entityManager,
+    ): Response
+    {
+
+        $contentieux = $participationRepository->find($id);
+        
+        $contentieux->setStatut('Ok');
+
+        $entityManager->persist($contentieux);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_contentieux');
+
+    }
+    
+    #[Route('/avis/u/{id}', name: 'app_valid_avis')]
+    public function edit(string $id, Request $request, DocumentManager $documentManager): Response
+    {
+
+        $avis = $documentManager->getRepository(Avis::class)->find($id);
+        
+        $avis->setModere(true);
+        $avis->setValide(true);
+
+        $documentManager->persist($avis);
+        $documentManager->flush();
+
+        return $this->redirectToRoute('app_avis');
+
+    }
+    
+}
---
+++ src/Document/Avis.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use App\Repository\AvisRepository;
+use App\Entity\User;
+
+#[MongoDB\Document(repositoryClass: AvisRepository::class)]
+class Avis
+{
+    #[MongoDB\Id]
+    protected string $id;
+
+    #[MongoDB\Field(type: 'int')]
+    protected int $chauffeur;
+
+    #[MongoDB\Field(type: 'string')]
+    protected string $avis;
+
+    #[MongoDB\Field(type: 'string')]
+    protected string $modere;
+
+    #[MongoDB\Field(type: 'string')]
+    protected string $valide;
+
+    #[MongoDB\Field(type: 'int')]
+    protected int $note;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getChauffeur(): ?int
+    {
+        return $this->chauffeur;
+    }
+
+    public function setChauffeur(int $chauffeur): static
+    {
+        $this->chauffeur = $chauffeur;
+
+        return $this;
+    }
+
+    public function getAvis(): ?string
+    {
+        return $this->avis;
+    }
+
+    public function setAvis(string $avis): static
+    {
+        $this->avis = $avis;
+
+        return $this;
+    }
+
+    public function getModere(): ?bool
+    {
+        return $this->modere;
+    }
+
+    public function setModere(bool $modere): static
+    {
+        $this->modere = $modere;
+
+        return $this;
+    }
+
+    public function getValide(): ?bool
+    {
+        return $this->valide;
+    }
+
+    public function setValide(bool $valide): static
+    {
+        $this->valide = $valide;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): static
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+}
